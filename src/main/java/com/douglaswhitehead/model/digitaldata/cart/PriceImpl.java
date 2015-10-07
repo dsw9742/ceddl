cf4d165abed5817f34042a8dc5451fdb715f7957
dsw9742 <dsw9742@hotmail.com>
2015-10-07 08:26:12 -0400

    Committing updates to PriceImpl and PriceImplTest.

--- src/main/java/com/douglaswhitehead/model/digitaldata/cart/PriceImpl.java
+++ src/main/java/com/douglaswhitehead/model/digitaldata/cart/PriceImpl.java
@@ -13,16 +13,65 @@ import com.douglaswhitehead.model.digitaldata.security.Security;
  */
 public class PriceImpl extends BaseImpl implements Price {
 
+	/**
+	 * String constant for <tt>basePrice</tt>.
+	 */
 	private final static String BASE_PRICE = "basePrice";
+	
+	/**
+	 * String constant for <tt>voucherCode</tt>.
+	 */
 	private final static String VOUCHER_CODE = "voucherCode";
+	
+	/**
+	 * String constant for <tt>voucherDiscount</tt>.
+	 */
 	private final static String VOUCHER_DISCOUNT = "voucherDiscount";
+	
+	/**
+	 * String constant for <tt>currency</tt>.
+	 */
 	private final static String CURRENCY = "currency";
+	
+	/**
+	 * String constant for <tt>taxRate</tt>.
+	 */
 	private final static String TAX_RATE = "taxRate";
+	
+	/**
+	 * String constant for <tt>shipping</tt>.
+	 */
 	private final static String SHIPPING = "shipping";
+	
+	/**
+	 * String constant for <tt>shippingMethod</tt>.
+	 */
 	private final static String SHIPPING_METHOD = "shippingMethod";
+	
+	/**
+	 * String constant for <tt>priceWithTax</tt>.
+	 */
 	private final static String PRICE_WITH_TAX = "priceWithTax";
+	
+	/**
+	 * String constant for <tt>cartTotal</tt>.
+	 */
 	private final static String CART_TOTAL = "cartTotal";
 	
+	/**
+	 * PriceImpl constructor.
+	 * 
+	 * @param Security security
+	 * @param BigDecimal basePrice
+	 * @param String voucherCode
+	 * @param BigDecimal voucherDiscount
+	 * @param String currency
+	 * @param BigDecimal taxRate
+	 * @param BigDecimal shipping
+	 * @param String shippingMethod
+	 * @param BigDecimal priceWithTax
+	 * @param BigDecimal cartTotal
+	 */
 	private PriceImpl(
 		final Security security,
 		final BigDecimal basePrice,
@@ -47,62 +96,137 @@ public class PriceImpl extends BaseImpl implements Price {
 		this.map.put(CART_TOTAL, cartTotal);
 	}
 	
+	/**
+	 * Returns the BasePrice object.
+	 * 
+	 * @return BigDecimal
+	 */
 	public BigDecimal getBasePrice() {
 		return (BigDecimal)map.get(BASE_PRICE);
 	}
 	
+	/**
+	 * Returns the VoucherCode object.
+	 * 
+	 * @return String
+	 */
 	public String getVoucherCode() {
 		return (String)map.get(VOUCHER_CODE);
 	}
 	
+	/**
+	 * Returns the VoucherDiscount object.
+	 * 
+	 * @return BigDecimal
+	 */
 	public BigDecimal getVoucherDiscount() {
 		return (BigDecimal)map.get(VOUCHER_DISCOUNT);
 	}
 	
+	/**
+	 * Returns the Currency object.
+	 * 
+	 * @return String
+	 */
 	public String getCurrency() {
 		return (String)map.get(CURRENCY);
 	}
 
+	/**
+	 * Returns the TaxRate object.
+	 * 
+	 * @return BigDecimal
+	 */
 	public BigDecimal getTaxRate() {
 		return (BigDecimal)map.get(TAX_RATE);
 	}
 
+	/**
+	 * Returns the Shipping object.
+	 * 
+	 * @return BigDecimal
+	 */
 	public BigDecimal getShipping() {
 		return (BigDecimal)map.get(SHIPPING);
 	}
 
+	/**
+	 * Returns the ShippingMethod object.
+	 * 
+	 * @return String
+	 */
 	public String getShippingMethod() {
 		return (String)map.get(SHIPPING_METHOD);
 	}
 
+	/**
+	 * Returns the PriceWithTax object.
+	 * 
+	 * @return BigDecimal
+	 */
 	public BigDecimal getPriceWithTax() {
 		return (BigDecimal)map.get(PRICE_WITH_TAX);
 	}
 
+	/**
+	 * Returns the CartTotal object. 
+	 * 
+	 * @return BigDecimal
+	 */
 	public BigDecimal getCartTotal() {
 		return (BigDecimal)map.get(CART_TOTAL);
 	}
 
+	/**
+	 * Implementation of the Price.Builder interface.
+	 * 
+	 * @author douglas whitehead
+	 *
+	 */
 	public static class Builder extends BaseImpl.Builder<Builder> implements Price.Builder {
 		
+		/**
+		 * Builds the BasePrice object.
+		 * 
+		 * @param BigDecimal BasePrice
+		 * @return Builder
+		 */
 		public Builder basePrice(final BigDecimal basePrice) {
 			this.map.put(BASE_PRICE, basePrice);
 			this.previous = BASE_PRICE;
 			return builder();
 		}
 		
+		/**
+		 * Builds the VoucherCode object.
+		 * 
+		 * @param String VoucherCode
+		 * @return Builder
+		 */
 		public Builder voucherCode(final String voucherCode) {
 			this.map.put(VOUCHER_CODE, voucherCode);
 			this.previous = VOUCHER_CODE;
 			return builder();
 		}
 		
+		/**
+		 * Builds the VoucherDiscount object.
+		 * 
+		 * @param BigDecimal VoucherDiscount
+		 * @return Builder
+		 */
 		public Builder voucherDiscount(final BigDecimal voucherDiscount) {
 			this.map.put(VOUCHER_DISCOUNT, voucherDiscount);
 			this.previous = VOUCHER_DISCOUNT;
 			return builder();
 		}
 		
+		/**
+		 * Builds the Currency object.
+		 * 
+		 * @param String currency
+		 * @return Builder
+		 */
 		public Builder currency(final String currency) {
 			this.map.put(CURRENCY, currency);
 			this.previous = CURRENCY;
--- src/test/java/com/douglaswhitehead/model/digitaldata/cart/PriceImplTest.java
+++ src/test/java/com/douglaswhitehead/model/digitaldata/cart/PriceImplTest.java
@@ -1,5 +1,41 @@
 package com.douglaswhitehead.model.digitaldata.cart;
 
+import java.math.BigDecimal;
+
+import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+import com.douglaswhitehead.model.digitaldata.cart.Price;
+import com.douglaswhitehead.model.digitaldata.cart.PriceImpl;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class PriceImplTest {
+	
+	private static final String VALID = "{\"basePrice\":200.00,\"voucherCode\":\"Alpha\",\"voucherDiscount\":0.50,\"taxRate\":0.20,\"shipping\":5.00,\"shippingMethod\":\"UPS\",\"priceWithTax\":120,\"cartTotal\":125,\"currency\":\"EUR\"}";
+	
+	@Test
+	public void test() throws Exception {
+		Price object = new PriceImpl.Builder()
+								.basePrice(new BigDecimal("200.00"))
+								.voucherCode("Alpha")
+								.voucherDiscount(new BigDecimal("0.50"))
+								.currency("EUR")
+								.taxRate(new BigDecimal("0.20"))
+								.shipping(new BigDecimal("5.00"))
+								.shippingMethod("UPS")
+								.priceWithTax(new BigDecimal("120"))
+								.cartTotal(new BigDecimal("125"))
+							.build();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setSerializationInclusion(Include.NON_NULL);
+		String output = mapper.writeValueAsString(object);
+		
+		System.out.println(VALID);
+		System.out.println(output);
+		
+		JSONAssert.assertEquals(VALID, output, false);
+	}
 
 }
