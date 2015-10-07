7ba5c2b9e9fae6c73296a1961d34580190120bba
dsw9742 <dsw9742@hotmail.com>
2015-10-07 09:29:21 -0400

    Committing updated documentation for CartImpl.

--- src/main/java/com/douglaswhitehead/model/digitaldata/cart/CartImpl.java
+++ src/main/java/com/douglaswhitehead/model/digitaldata/cart/CartImpl.java
@@ -13,11 +13,35 @@ import com.douglaswhitehead.model.digitaldata.security.Security;
  */
 public class CartImpl extends BaseImpl implements Cart {
 
+	/**
+	 * String constant for <tt>cartID</tt>.
+	 */
 	private final static String CART_ID = "cartID";
+	
+	/**
+	 * String constant for <tt>price</tt>.
+	 */
 	private final static String PRICE = "price";
+	
+	/**
+	 * String constant for <tt>attributes</tt>.
+	 */
 	private final static String ATTRIBUTES = "attributes";
+	
+	/**
+	 * String constant for <tt>item</tt>.
+	 */
 	private final static String ITEM = "item";
 	
+	/**
+	 * CartImpl constructor.
+	 * 
+	 * @param Security security
+	 * @param String cartID
+	 * @param Price price
+	 * @param Attributes attributes
+	 * @param Item[] item
+	 */
 	private CartImpl(
 		final Security security,
 		final String cartID,
@@ -32,48 +56,99 @@ public class CartImpl extends BaseImpl implements Cart {
 		this.map.put(ITEM, item);
 	}
 	
+	/**
+	 * Returns the String object.
+	 * 
+	 * @return String
+	 */
 	public String getCartID() {
 		return (String)map.get(CART_ID);
 	}
 	
+	/**
+	 * Returns the Price object.
+	 * 
+	 * @return Price
+	 */
 	public Price getPrice() {
 		return (Price)map.get(PRICE);
 	}
 	
+	/**
+	 * Returns the Attributes object.
+	 * 
+	 * @return Attributes
+	 */
 	public Attributes getAttributes() {
 		return (Attributes)map.get(ATTRIBUTES);
 	}
 	
+	/**
+	 * Returns the Item array.
+	 * 
+	 * @return Item[]
+	 */
 	public Item[] getItem() {
 		return (Item[])map.get(ITEM);
 	}
 	
+	/**
+	 * Implementation of the Cart.Builder interface.
+	 * 
+	 * @author douglas whitehead
+	 *
+	 */
 	public static class Builder extends BaseImpl.Builder<Builder> implements Cart.Builder {
 
+		/**
+		 * Builds the CartID object.
+		 * 
+		 * @return Builder
+		 */
 		public Builder cartID(final String cartID) {
 			this.map.put(CART_ID, cartID);
 			this.previous = CART_ID;
 			return builder();
 		}
 		
+		/**
+		 * Builds the Price object.
+		 * 
+		 * @return Builder
+		 */
 		public Builder price(final Price price) {
 			this.map.put(PRICE, price);
 			this.previous = PRICE;
 			return builder();
 		}
 
+		/**
+		 * Builds the Attributes object.
+		 * 
+		 * @return Builder
+		 */
 		public Builder attributes(final Attributes attributes) {
 			this.map.put(ATTRIBUTES, attributes);
 			this.previous = ATTRIBUTES;
 			return builder();
 		}
 		
+		/**
+		 * Builds the Item array.
+		 * 
+		 * @return Builder
+		 */
 		public Builder item(final Item[] item) {
 			this.map.put(ITEM, item);
 			this.previous = ITEM;
 			return builder();
 		}
 		
+		/**
+		 * Builds and returns the CartImpl object.
+		 * 
+		 * @return CartImpl
+		 */
 		public CartImpl build() {
 			return new CartImpl(
 				security,
