0f9cf2e3a8e78715042eed0120b16292a94e2f4e
Doug Whitehead <dsw9742@hotmail.com>
2015-10-12 09:24:59 -0400

    Additional documentation on AccessCategory.

--- src/main/java/com/douglaswhitehead/model/digitaldata/privacy/AccessCategory.java
+++ src/main/java/com/douglaswhitehead/model/digitaldata/privacy/AccessCategory.java
@@ -13,16 +13,37 @@ package com.douglaswhitehead.model.digitaldata.privacy;
 public interface AccessCategory {
 	
 	/**
-	 * Returns the CategoryName object.
+	 * <p>Returns the CategoryName object.</p>
+	 * 
+	 * <p>From the W3C CEDDL specification:</p>
+	 * 
+	 * <p>Definition of category names to be associated with <tt>security</tt> objects with the matching name.</p>
+	 * 
+	 * <p>
+	 * A <tt>categoryName</tt> of Default MUST be included for privacy enforcement. All other categories
+     * are custom. Common categories include Analytics, Recommendations, and Personalization.
+     * </p>
 	 * 
 	 * @return String
 	 */
 	public String getCategoryName();
 	
 	/**
-	 * Returns the Domains array.
+	 * <p>Returns the Domains array.</p>
 	 * 
-	 * @return String[]
+	 * <p>From the W3C CEDDL specification:</p>
+	 * 
+	 * <p>
+	 * Particular vendors associated with the particular category. This SHOULD be an array listing
+	 * domains for vendors associated with the category.
+	 * </p>
+	 * 
+	 * <p>
+	 * The Default category SHOULD contain all technologies which must be treated the same as first
+     * party for privacy purposes (e.g., a tag management technology).
+     * </p>
+	 * 
+	 * @return String
 	 */
 	public String[] getDomains();
 	
